c7bd880c4514de7158ebf8762524d2a22f61a301
ayeshaa1234 <113435751+ayeshaa1234@users.noreply.github.com>
2022-09-13 16:08:36 +0500

    Add files via upload

---
+++ act 1.cpp
@@ -0,0 +1,14 @@
+
+#include<iostream>
+using namespace std;
+int main()
+
+{
+	cout << "hello world!";
+	int a;
+	cout<<"enter the value of a";
+	cin>>a;
+	cout<<a;
+	return 0;
+}
+
---
+++ bai 019 lab 1 activities/act3.cpp
@@ -0,0 +1,14 @@
+
+ 
+
+
+#include <iostream> 
+using namespace std; 
+int main () 
+{ 
+int x; 
+ cout << "Type a number: "; 
+ cin>>x;
+ cout<<"your num is:"<<x;
+return 0;
+}
